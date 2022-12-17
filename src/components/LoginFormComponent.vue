7237e60f43ccb45eed6c9965a1648860e5b5b772
Rafael Masallo <rafaelmasallo@gmail.com>
2022-12-18 01:18:45 +0800

    🖼️ finalized UI for login and 🎣 handle onSubmit event with validation messages

--- src/components/LoginFormComponent.vue
+++ src/components/LoginFormComponent.vue
@@ -1,10 +1,46 @@
-<template>LoginFormComponent</template>
+<template>
+    <form class="needs-validation" novalidate @submit="handleOnSubmit">
+        <div class="row g-3">
+            <div class="col-md-12">
+                <label for="username" class="form-label">Username</label>
+                <input type="text" class="form-control" id="username" placeholder="" value="" required />
+                <div class="invalid-feedback">username is required.</div>
+            </div>
+
+            <div class="col-md-12">
+                <label for="password" class="form-label">Password</label>
+                <input type="password" class="form-control" id="password" placeholder="" value="" required />
+                <div class="invalid-feedback">password is required.</div>
+            </div>
+        </div>
+
+        <hr class="my-4" />
+
+        <button class="w-100 btn btn-primary btn-lg" type="submit">Login</button>
+    </form>
+</template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
 
 export default defineComponent({
     name: 'LoginFormComponent',
+    methods: {
+        handleOnSubmit(event: Event) {
+            event.preventDefault();
+            if (event.target) {
+                const element = event.target as HTMLFormElement;
+                const isValid = element.checkValidity();
+
+                if (!isValid) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+
+                element.classList.add('was-validated');
+            }
+        },
+    },
 });
 </script>
 
