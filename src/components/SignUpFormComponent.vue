5793ed92c167adff3765c7f58b32892a4b5f4b13
Rafael Masallo <rafaelmasallo@gmail.com>
2022-12-18 03:12:11 +0800

    🔥 initialized firebase and 🏗️ implemented email authentication

--- src/components/SignUpFormComponent.vue
+++ src/components/SignUpFormComponent.vue
@@ -3,13 +3,13 @@
         <div class="row g-3">
             <div class="col-md-12">
                 <label for="email" class="form-label">Email</label>
-                <input type="email" class="form-control" id="email" placeholder="" value="" required />
+                <input v-model="email" type="email" class="form-control" id="email" placeholder="" required />
                 <div class="invalid-feedback">email is required.</div>
             </div>
 
             <div class="col-md-12">
                 <label for="password" class="form-label">Password</label>
-                <input type="password" class="form-control" id="password" placeholder="" value="" required />
+                <input v-model="password" type="password" class="form-control" id="password" placeholder="" required />
                 <div class="invalid-feedback">password is required.</div>
             </div>
         </div>
@@ -22,9 +22,13 @@
 
 <script lang="ts">
 import { defineComponent } from 'vue';
+import { getAuth, createUserWithEmailAndPassword } from 'firebase/auth';
 
 export default defineComponent({
     name: 'SignUpFormComponent',
+    data() {
+        return { email: '', password: '' };
+    },
     methods: {
         handleOnSubmit(event: Event) {
             event.preventDefault();
@@ -35,6 +39,17 @@ export default defineComponent({
                 if (!isValid) {
                     event.preventDefault();
                     event.stopPropagation();
+                } else {
+                    console.log(this.email);
+                    console.log(this.password);
+
+                    createUserWithEmailAndPassword(getAuth(), this.email, this.password)
+                        .then(() => {
+                            alert('successfully registered');
+                        })
+                        .catch(() => {
+                            alert('try again');
+                        });
                 }
 
                 element.classList.add('was-validated');
--- src/main.ts
+++ src/main.ts
@@ -5,5 +5,6 @@ import App from './App.vue';
 import './registerServiceWorker';
 import router from './router';
 import store from './store';
+import './firebase';
 
 createApp(App).use(store).use(router).mount('#app');
