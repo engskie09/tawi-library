41db9f95da9b30ab30060ed225ea49edc9c0b024
Rafael Masallo <rafaelmasallo@gmail.com>
2022-12-17 23:51:10 +0800

    📚 resetup project standard settings with initial files

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,30 @@
 <template>
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <LibraryPage />
-    <LoginPage />
+    <nav>
+        <router-link to="/">Home</router-link> |
+        <router-link to="/about">About</router-link>
+    </nav>
+    <router-view />
 </template>
 
-<script>
-import LibraryPage from './components/page/library.vue';
-import LoginPage from './components/page/login.vue';
-
-export default {
-    name: 'App',
-    components: {
-        LibraryPage,
-        LoginPage,
-    },
-};
-</script>
-
-<style>
+<style lang="scss">
 #app {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
     text-align: center;
     color: #2c3e50;
-    margin-top: 60px;
+}
+
+nav {
+    padding: 30px;
+
+    a {
+        font-weight: bold;
+        color: #2c3e50;
+
+        &.router-link-exact-active {
+            color: #42b983;
+        }
+    }
 }
 </style>
